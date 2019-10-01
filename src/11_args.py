08db8d31d25f0974dd4b464ed62ee8c5f7be9697
Cai Martin <cmartin.professional@yahoo.com>
2019-10-01 16:33:53 -0400

    finished page 11

--- src/11_args.py
+++ src/11_args.py
@@ -5,29 +5,43 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
-
-print(f1(1, 2))
+def f1(num1,num2):
+    return num1 + num2
+print("f1",f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
 
-print(f2(1))                    # Should print 1
-print(f2(1, 3))                 # Should print 4
-print(f2(1, 4, -12))            # Should print -7
-print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
+def f2(*args):
+    result = 0
+    for v in args:
+        if isinstance(v, list):
+            for x in v:
+                result += x
+        else:
+            result += v
+    return result
+
+print("f2",f2(1))                    # Should print 1
+print("f2",f2(1, 3))                 # Should print 4
+print("f2",f2(1, 4, -12))            # Should print -7
+print("f2",f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
 print(f2(a))    # Should print 22
 
-# Write a function f3 that accepts either one or two arguments. If one argument,
+# Write a function f3 that accepts either one or two arguments. If one argument,s
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+
+def f3(num1,num2=1):
+    return num1+num2
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -42,7 +56,9 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
-
+def f4(**kwargs):
+    for x in kwargs.items():
+        print("key:",x[0],"value:",x[1])
 # Should print
 # key: a, value: 12
 # key: b, value: 30
@@ -58,6 +74,5 @@ d = {
     "monster": "goblin",
     "hp": 3
 }
-
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
